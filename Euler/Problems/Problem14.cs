c8dae526629f97a9b66155504736007a06582b23
Josh Doherty-Turnbull <king_j3181990@hotmail.com>
2016-04-02 17:11:27 +0100

    Problem fourteen done

---
+++ Euler/Problems/Problem14.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Problems
+{
+    class Problem14 : BaseProblem
+    {
+        public override string ProblemDescription
+        {
+            get
+            {
+                return @"The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.";
+            }
+        }
+
+        public override string ProblemName
+        {
+            get
+            {
+                return @"Longest Collatz sequence";
+            }
+        }
+
+        public override int ProblemNumber
+        {
+            get
+            {
+                return 14;
+            }
+        }
+
+        protected override string Solve()
+        {
+            long maxStart = 0, maxChain = 0;
+
+            for (int i = 2; i < 1000000; i++)
+            {
+                long n = i, chain = 0;
+                do
+                {
+                    if (n % 2 == 0)
+                    {
+                        n /= 2;
+
+                    }
+                    else
+                    {
+                        n = n * 3 + 1;
+                    }
+                    chain++;
+
+
+                } while (n != 1);
+                if (chain > maxChain)
+                {
+                    maxStart = i;
+                    maxChain = chain;
+                }
+            }
+            return maxStart.ToString();
+        }
+
+
+    }
+}
